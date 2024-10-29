6fad3766c37e9d628ff6a3d970c9283968c66c99
Hydrateacher <112601665+Hydrateacher@users.noreply.github.com>
2024-10-29 18:37:03 +0500

    Add files via upload

---
+++ animation.py
@@ -0,0 +1,16 @@
+from turtle import *
+import colorsys as colorsys
+bgcolor("black")
+speed(0)
+tracer(5)
+hideturtle()
+hue=0.0
+for i in range(500):
+    col=colorsys.hsv_to_rgb(hue,1,1)
+    pencolor(col)
+    fd(i)
+    lt(144)
+    circle(5)
+    rt(80)
+    hue+=1/720
+done()
